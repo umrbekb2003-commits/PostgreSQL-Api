4d32d67d2bc2e649d4fd5c9e70dc680c4429b0d4
Umrbek <umrbekb2003@gmail.com>
2026-03-30 17:33:47 +0000

    new folder

---
+++ person_and_accound_db/README.md
@@ -0,0 +1,1 @@
+This .sql file shows the two tables being linked in practice, with comments included.
---
+++ person_and_accound_db/users.sql
@@ -0,0 +1,7 @@
+create table users (id serial primary key, first varchar(100), name varchar(100), age int ); --A table named user was created, containing the columns surname, first_name, and age.
+
+create table emails(id serial primary key, email varchar(100), users_id int references users(id)); --A table named emails was created, which has a column email and is linked to the users table through the users_id column.
+
+insert into users (first, name, age) values('Bobomurodov', 'Umrbek', 22); -- The current view of the data that was added through the command to insert data into the users table.
+
+select u.first, u.name. u.age, e.email from users u join emails e on u.id = e.users_id; -- In this case, the two tables are connected, with the users table linked by id and the emails table linked by users_id.
