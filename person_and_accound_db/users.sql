166808f912806bb1f3652142345d01171df2190a
Umrbek <umrbekb2003@gmail.com>
2026-03-30 17:35:48 +0000

    remove sql file

--- person_and_accound_db/users.sql
+++ person_and_accound_db/users.sql
@@ -1,6 +1,6 @@
 create table users (id serial primary key, first varchar(100), name varchar(100), age int ); --A table named user was created, containing the columns surname, first_name, and age.
 
-create table emails(id serial primary key, email varchar(100), users_id int references users(id)); --A table named emails was created, which has a column email and is linked to the users table through the users_id column.
+create table emails(id serial primary key, email varchar(100) unique , users_id int references users(id)); --A table named emails was created, which has a column email and is linked to the users table through the users_id column.
 
 insert into users (first, name, age) values('Bobomurodov', 'Umrbek', 22); -- The current view of the data that was added through the command to insert data into the users table.
 
